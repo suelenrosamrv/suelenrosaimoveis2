2d4e2a36ea315d9d3389fe08eccad4dbb6e5fd5b
Suelen Rosa <suelenrosa.mrv@gmail.com>
2025-07-23 00:49:49 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# SR Negócios Imobiliários
+
+Site institucional para investidores, construído com Next.js.
---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>SR Negócios Imobiliários</h1>
+      <p>Site oficial em construção. Em breve novidades para investidores!</p>
+    </div>
+  );
+}
